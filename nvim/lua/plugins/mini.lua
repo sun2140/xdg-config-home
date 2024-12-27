167d70e4d122bb468be8be57a61ed1efbe40eda1
Nicolas GORAJ <github@goraj.fr>
2024-12-27 21:40:27 +0100

    feat: configuring alacritty and theme

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -13,12 +13,12 @@ return {
             MiniFiles.open()
         end, { desc = '[E]xplore filesystem' })
 
-        local statusline = require 'mini.statusline'
-        statusline.setup { use_icons = vim.g.have_nerd_font }
+        --           local statusline = require 'mini.statusline'
+        --           statusline.setup { use_icons = vim.g.have_nerd_font }
 
-        ---@diagnostic disable-next-line: duplicate-set-field
-        statusline.section_location = function()
-            return '%2l:%-2v'
-        end
+        --           ---@diagnostic disable-next-line: duplicate-set-field
+        --           statusline.section_location = function()
+        --               return '%2l:%-2v'
+        --           end
     end,
 }
